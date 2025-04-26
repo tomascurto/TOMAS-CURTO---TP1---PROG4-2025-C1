816258ae5d7210afd7f24d7c9f009aa70e28f971
Tomas <tomascurto1@gmail.com>
2025-04-26 12:59:39 -0300

    css y html login y registro

--- Sala_de_Juegos/src/app/pages/login/login.component.ts
+++ Sala_de_Juegos/src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  loginError = '';
 
+  onLogin() {
+    // Lógica de login con Firebase acá
+    console.log('Login con', this.email, this.password);
+  }
+
+  quickLogin(email: string) {
+    const fakePassword = '123456'; // Cambiar según tus usuarios de prueba
+    this.email = email;
+    this.password = fakePassword;
+    this.onLogin();
+  }
 }
--- Sala_de_Juegos/src/app/pages/registro/registro.component.ts
+++ Sala_de_Juegos/src/app/pages/registro/registro.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-registro',
-  imports: [],
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  nombre = '';
+  apellido = '';
+  edad: number | null = null;
+  email = '';
+  password = '';
+  registerError = '';
 
+  onRegister() {
+    // Lógica de registro con Firebase acá
+    console.log('Registro de', this.nombre, this.apellido, this.edad, this.email);
+  }
 }
