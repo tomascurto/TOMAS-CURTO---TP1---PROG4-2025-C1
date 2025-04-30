fd266e47c43a2b44c376bb8d14d97b055e71eaa1
Tomas <tomascurto1@gmail.com>
2025-04-30 01:27:02 -0300

    enviroment fix

--- Sala_de_Juegos/src/main.ts
+++ Sala_de_Juegos/src/main.ts
@@ -7,7 +7,7 @@ import { routes } from './app/app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from './enviroments/enviroment';
+import { environment } from './environments/environment';
 
 
 bootstrapApplication(AppComponent, {
