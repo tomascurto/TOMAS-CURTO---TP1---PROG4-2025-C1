dd5353b5b6a23a012bc8f1437a2596a74587fb29
Tomas <tomascurto1@gmail.com>
2025-05-03 19:15:45 -0300

    template front home

--- Sala_de_Juegos/src/app/pages/home/home.component.ts
+++ Sala_de_Juegos/src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterModule, CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  games = [
+    { name: 'Juego 1', description: 'descripcion juego 1' },
+    { name: 'Juego 2', description: 'descripcion juego 2' },
+    { name: 'Juego 3', description: 'descripcion juego 3' },
+    { name: 'Juego 4', description: 'descripcion juego 4' }
+  ];
 }
